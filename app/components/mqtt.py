c16842238fdd5af5a0a2ae5262847737f6c88289
stanman71 <stanlay@gmx.net>
2019-03-23 16:21:38 +0100

    add mqtt publish

--- app/components/mqtt.py
+++ app/components/mqtt.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 
+BROKER_ADDRESS = "localhost"
+
 def MQTT_START():
  
 	def on_message(client, userdata, message):
@@ -9,9 +11,7 @@ def MQTT_START():
 	 
 	def on_connect(client, userdata, flags, rc):
 		client.subscribe('/SmartHome/data')
-	 
-	BROKER_ADDRESS = "localhost"
-	 
+
 	client = mqtt.Client()
 	client.on_connect = on_connect
 	client.on_message = on_message
@@ -21,3 +21,15 @@ def MQTT_START():
 	print("Connected to MQTT Broker: " + BROKER_ADDRESS)
 	 
 	client.loop_forever()
+
+
+def MQTT_PUBLISH(MQTT_TOPIC, MQTT_MSG):
+
+	def on_publish(client, userdata, mid):
+		print ("Message Published...")
+
+	client = mqtt.Client()
+	client.on_publish = on_publish
+	client.connect(BROKER_ADDRESS) 
+	client.publish(MQTT_TOPIC,MQTT_MSG)
+	client.disconnect()
--- app/sites/schedular.py
+++ app/sites/schedular.py
@@ -10,6 +10,7 @@ from app.components.sensors_control import *
 from app.components.plants_control import *
 from app.database.database import *
 from app.components.tasks import SCHEDULAR_TASKS
+from app.components.mqtt import MQTT_PUBLISH
 
 
 # create role "superuser"
@@ -88,6 +89,7 @@ def dashboard_schedular():
                              "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36","37",
                              "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48","49", "50",
                              "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+
 
     return render_template('dashboard_schedular.html',
                             dropdown_list_days=dropdown_list_days,
